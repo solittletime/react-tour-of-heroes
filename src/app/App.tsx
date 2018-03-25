9018187e36d7eadb7fc7a8068ea5d9e380b7afa4
solittletime <solittletime@users.noreply.github.com>
2018-03-25 11:57:04 -0400

    tour of heroes - part 0

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    const title = 'Tour of Heroes';
+    return (
+      <h1>{title}</h1>
+    );
+  }
+}
+
+export default App;
